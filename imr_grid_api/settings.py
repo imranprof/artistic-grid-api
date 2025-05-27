99ff40ca5d132ab6682225348aafc116b724dd63
imranprof <imranpers@gmail.com>
2025-05-28 00:55:16 +0600

    chore: add rest jwt as default authentication class

--- imr_grid_api/settings.py
+++ imr_grid_api/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'accounts.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 
 
 # Password validation
