34adc4047b2172c3da33fde36609b2cd95eb1958
imranprof <imranpers@gmail.com>
2025-05-28 00:47:43 +0600

    feat: add signup and login, view and serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 class UserSignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators = [validate_password])
@@ -16,3 +19,51 @@ class UserSignupSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = 'username'
+
+    def validate(self, attrs):
+        username_or_email = attrs.get("username")
+        password = attrs.get("password")
+
+        #allow login with either username or email
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=username_or_email,
+            password=password
+        )
+
+        if user is None:
+            # Try authenticate with email
+            try:
+                user_obj = User.objects.get(email=username_or_email)
+                user = authenticate(
+                    request=self.context.get("request"),
+                    username=user_obj.username,
+                    password=password
+                )
+            except User.DoesNotExist:
+                pass
+        
+        if user is None:
+            raise serializers.ValidationError(
+                "Invalid username or password"
+            )
+        
+        data = super().validate({
+            'username': user.username,
+            'password': password
+        })
+
+        #Add custom user data to the response
+
+        data["user"] = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        }
+
+        return data
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSignupSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .serializers import CustomTokenObtainPairSerializer
 
-# Create your views here.
+class SignupView(APIView):
+    def post(self, request):
+        serializer = UserSignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
