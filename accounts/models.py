4ad849814b3e8d9fc1bee03f25a81280e997403a
imranprof <imranpers@gmail.com>
2025-05-28 00:24:19 +0600

    feat: add serializer for signup

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-  email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
 
-  def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
 
 
