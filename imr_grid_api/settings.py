a0f87752b8ed95fba9df49839054ca74f3d52671
imranprof <imranpers@gmail.com>
2025-10-09 05:23:01 +0600

    feat: migrate model to db

--- imr_grid_api/settings.py
+++ imr_grid_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'accounts',
+    'imr_grid_api.apps.ImrGridApiConfig',
     'django_extensions',
     'corsheaders',
 ]
